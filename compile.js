07bceb6320db5e6203b6994d75572d0f9a05820b
Nimesh Solanki <nimesh3536@gmail.com>
2018-09-09 17:38:05 +0530

    compile.js code

---
+++ compile.js
@@ -0,0 +1,8 @@
+const path = require("path");
+const fs = require("fs");
+const solc = require("solc");
+
+const busSystemPath = path.resolve(__dirname, "contracts", "busSystem.sol");
+const source = fs.readFileSync(busSystemPath, "utf-8");
+
+module.exports = solc.compile(source, 1).contracts[":System"];
