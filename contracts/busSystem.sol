aeb000215628e2dfb88bb2ae4dd3aefe4cfda4ce
Nimesh Solanki <nimesh3536@gmail.com>
2018-09-15 19:46:12 +0530

    solve almost all errors

--- contracts/busSystem.sol
+++ contracts/busSystem.sol
@@ -5,40 +5,52 @@ contract System{
     event SenderLogger(address);
     event ValueLogger(uint); 
 
-    function () payable{
-        SenderLogger(msg.sender);
-        ValueLogger(msg.value);
-    }
-
+    address private admin;
+    uint public count=0;
+    uint priceOfTicket = 2;
+    
     struct Ticket{
-        string  token;
+        bytes32  token;
         string  fromPlace;
         string  toPlace;
         uint price;
     }
-    address public admin;
-    uint public count=0;
-    uint priceOfTicket = 2;
-    
+
     constructor() public{
         admin = msg.sender;
+        // Ticket storage newTicket;
         count++;
     }
 
+    // modifer checkOwner {
+    //     require(admin == msg.sender);
+    //     _;
+    // }
+
+
     function getTicket() public payable returns(Ticket) { 
         /* returns unique ticket token number */
-        return Ticket({token: uint(keccak256(block.difficulty, now, count)), 
-        fromPlace: 'A', 
-        toPlace: "B", 
-        price: 100});
-        
+        Ticket storage newTicket = {
+            token:keccak256(abi.encodePacked(block.timestamp, block.difficulty,count)),
+            fromPlace: 'A',
+            toPlace: 'B',
+            price: 10
+        };
+        emit SenderLogger(msg.sender);
+        emit ValueLogger(msg.value);
+        // return newTicket({token: uint(keccak256(block.difficulty, now, count)), 
+        // fromPlace: 'A', 
+        // toPlace: "B", 
+        // price: 100});
+
+        // newTicket.token = keccak256(abi.encodePacked(block.timestamp, block.difficulty,count));
     }
 
-    function getTicket() public payable{
-        /* returns all the details of the ticket including token number in one object*/
-        require(msg.value > priceOfTicket);
+    // function getTicket() public payable{
+    //     /* returns all the details of the ticket including token number in one object*/
+    //     require(msg.value > priceOfTicket);
 
-    }
+    // }
 
     function verifyToken() public view returns(bool){
     /* a function to verify token */
