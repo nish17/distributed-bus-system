309c3d5acf3327b2ffbee783eace4dd2324d26f0
Nimesh Solanki <nimesh3536@gmail.com>
2018-09-15 19:28:48 +0530

    try artifacts but didnt work

--- compile.js
+++ compile.js
@@ -6,3 +6,6 @@ const busSystemPath = path.resolve(__dirname, "contracts", "busSystem.sol");
 const source = fs.readFileSync(busSystemPath, "utf-8");
 
 module.exports = solc.compile(source, 1).contracts[":System"];
+
+// const project = artifacts.require("./contracts/busSystem.sol");
+// console.log(project);
