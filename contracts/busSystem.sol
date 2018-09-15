6ad9bdf9051c6f6bb28800d7cddb939aa98006cb
Nimesh Solanki <nimesh3536@gmail.com>
2018-09-16 02:03:01 +0530

    final changes

--- contracts/busSystem.sol
+++ contracts/busSystem.sol
@@ -8,36 +8,23 @@ contract System{
     address private admin;
     uint public count=0;
     uint priceOfTicket = 2;
-    bytes32  token;
+    bytes32 token;
     
     constructor() public{
+    /* Whenever the contract is deployed for the first time constructor is called and the address is stored in admin variable */
         admin = msg.sender;
         count++;
     }
-
-    // modifer checkOwner {
-    //     require(admin == msg.sender);
-    //     _;
-    // }
-    function getToken() public payable returns(bytes32) { 
+    function generateToken() public payable { 
         /* returns unique ticket token number */
         if(msg.value == 10 ether){
             token = keccak256(abi.encodePacked(block.timestamp, block.difficulty,count));
             emit SenderLogger(msg.sender);
             emit ValueLogger(msg.value);
-            return token;
         }
     }
-
-    // function getTicket() public payable{
-    //     /* returns all the details of the ticket including token number in one object*/
-    //     require(msg.value > priceOfTicket);
-
-    // }
-
-    // function verifyToken() public view returns(bool){
-    // /* a function to verify token */
-    //     // return 
-    // }
+    function getToken() public view returns(bytes32){
+        return token;
+    }
 
 }
