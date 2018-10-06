5e68663b7b4289b95ca460eef5495fc683ce9d22
Nimesh Solanki <nimesh3536@gmail.com>
2018-10-06 18:35:20 +0530

    update for deployment

--- compile.js
+++ compile.js
@@ -4,8 +4,11 @@ const solc = require("solc");
 
 const Route = path.resolve(__dirname, "contracts", "Route.sol");
 const source = fs.readFileSync(Route, "utf-8");
-// console.log(solc.compile(source, 1));
-module.exports = solc.compile(source, 1).contracts[":Route"];
+// console.log(solc.compile(source, 2));
+module.exports = {
+  route: solc.compile(source, 2).contracts[":Route"],
+  routeCreator: solc.compile(source, 2).contracts[":RouteCreator"]
+};
 
 // const project = artifacts.require("./contracts/busSystem.sol");
 // console.log(project);
--- deploy.js
+++ deploy.js
@@ -1,24 +1,70 @@
 const HDWallerProvider = require("truffle-hdwallet-provider");
 const Web3 = require("web3");
-const { interface, bytecode } = require("./compile");
+const { route, routeCreator } = require("./compile");
+// console.log(route.bytecode);
+// console.log(route.interface);
+const fs = require("fs");
+const RInterface = route.interface;
+const RByte = route.bytecode;
+const RCInterface = routeCreator.interface;
+const RCByte = routeCreator.bytecode;
 
 const provider = new HDWallerProvider(
   "december bomb print venue quantum balcony model slogan trouble blade arena believe",
-  "https://rinkeby.infura.io/v3/aa18450592954bd99ed379aa3c6ee17f"
+  "https://rinkeby.infura.io/v3/232565dedf3b408ea93b991ed92c70c4"
 );
 
 const web3 = new Web3(provider);
 
-const deploy = async () => {
+// const deployRouteCreatorContract = async () => {
+//   const accounts = await web3.eth.getAccounts();
+//   console.log(`Attempting to deploy from account ${accounts[0]}`);
+//   let result;
+//   try {
+//     result = await new web3.eth.Contract(JSON.parse(RCInterface))
+//       .deploy({
+//         data: RCByte
+//       })
+//       .send({ from: accounts[0], gas: "6000000" });
+//   } catch (e) {
+//     console.log(`Something went wrong:: ${e}`);
+//   }
+//   console.log(
+//     `Route has been successfully deployed to ${result.options.address}`
+//   );
+//   fs.appendFile(
+//     "./logs.txt",
+//     `Route Creator Contract deployed to ${result.options.address}`,
+//     err => {
+//       if (err) console.log(`something went wrong while writing into the file`);
+//       else console.log(`Success!`);
+//     }
+//   );
+// };
+// deployRouteCreatorContract();
+
+const deployRouteContract = async () => {
   const accounts = await web3.eth.getAccounts();
+  let result;
   console.log(`Attempting to deploy from account ${accounts[0]}`);
-
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({
-      data: bytecode
-    })
-    .send({ from: accounts[0], gas: "6000000" });
-
-  console.log(`Contract deployed to ${result.options.address}`);
+  try {
+    result = await new web3.eth.Contract(JSON.parse(RInterface))
+      .deploy({
+        data: RByte,
+        arguments: [accounts[0], "r1", 3, ["0x0stop1", "0x0stop2", "0x0stop3"]]
+      })
+      .send({ from: accounts[0], gas: "6000000" });
+  } catch (e) {
+    console.log(`Something went wrong:: ${e}`);
+  }
+  console.log(`Route Contract deployed to ${result.options.address}`);
+  fs.appendFile(
+    "./logs.txt",
+    `Route Contract deployed to ${result.options.address}`,
+    err => {
+      if (err) console.log(`something went wrong while writing into the file`);
+      else console.log(`Success!`);
+    }
+  );
 };
-deploy();
+deployRouteContract();
