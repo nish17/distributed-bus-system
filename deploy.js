c5442389f773f7413b047a41fdb137dbca13b475
Nimesh Solanki <nimesh3536@gmail.com>
2018-10-07 16:28:10 +0530

    all bugs fixed

--- deploy.js
+++ deploy.js
@@ -4,6 +4,7 @@ const { route, routeCreator } = require("./compile");
 // console.log(route.bytecode);
 // console.log(route.interface);
 const fs = require("fs");
+const date = new Date();
 const RInterface = route.interface;
 const RByte = route.bytecode;
 const RCInterface = routeCreator.interface;
@@ -16,34 +17,34 @@ const provider = new HDWallerProvider(
 
 const web3 = new Web3(provider);
 
-const deployRouteCreatorContract = async () => {
-  const accounts = await web3.eth.getAccounts();
-  console.log(`Attempting to deploy from account ${accounts[0]}`);
-  let result;
-  try {
-    result = await new web3.eth.Contract(JSON.parse(RCInterface))
-      .deploy({
-        data: RCByte
-      })
-      .send({ from: accounts[0], gas: "6000000" });
-  } catch (e) {
-    console.log(`Something went wrong:: ${e}`);
-  }
-  console.log(
-    `Route Creator Contract has been successfully deployed to ${
-      result.options.address
-    }`
-  );
-  fs.appendFile(
-    "./logs.txt",
-    `Route Creator Contract deployed to ${result.options.address}\r\n`,
-    err => {
-      if (err) console.log(`something went wrong while writing into the file`);
-      else console.log(`Success!`);
-    }
-  );
-};
-deployRouteCreatorContract();
+// const deployRouteCreatorContract = async () => {
+//   const accounts = await web3.eth.getAccounts();
+//   console.log(`Attempting to deploy from account ${accounts[0]}`);
+//   let result;
+//   try {
+//     result = await new web3.eth.Contract(JSON.parse(RCInterface))
+//       .deploy({
+//         data: RCByte
+//       })
+//       .send({ from: accounts[0], gas: "6000000" });
+//   } catch (e) {
+//     console.log(`Something went wrong:: ${e}`);
+//   }
+//   console.log(
+//     `Route Creator Contract has been successfully deployed to ${
+//       result.options.address
+//     }`
+//   );
+//   fs.appendFile(
+//     "./logs.txt",
+//     `Route Creator Contract deployed to ${result.options.address} on ${date.toLocaleTimeString}\r\n`,
+//     err => {
+//       if (err) console.log(`something went wrong while writing into the file`);
+//       else console.log(`Success!`);
+//     }
+//   );
+// };
+// deployRouteCreatorContract();
 
 const deployRouteContract = async () => {
   const accounts = await web3.eth.getAccounts();
@@ -53,7 +54,16 @@ const deployRouteContract = async () => {
     result = await new web3.eth.Contract(JSON.parse(RInterface))
       .deploy({
         data: RByte,
-        arguments: [accounts[0], "r1", 3, ["0x0stop1", "0x0stop2", "0x0stop3"]]
+        arguments: [
+          accounts[0],
+          "r1",
+          3,
+          [
+            web3.utils.fromAscii("stop1"),
+            web3.utils.fromAscii("stop2"),
+            web3.utils.fromAscii("stop3")
+          ]
+        ]
       })
       .send({ from: accounts[0], gas: "2000000" });
   } catch (e) {
@@ -64,7 +74,9 @@ const deployRouteContract = async () => {
   );
   fs.appendFile(
     "./logs.txt",
-    `Route Contract deployed to ${result.options.address}\r\n`,
+    `Route Contract deployed to ${result.options.address} on ${
+      date.toLocaleTimeString
+    }\r\n`,
     err => {
       if (err) console.log(`something went wrong while writing into the file`);
       else console.log(`Success!`);
