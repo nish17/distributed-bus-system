f24ceecc518c1a162b938bfed374eea7b69aa55d
nish17 <nimesh3536@gmail.com>
2018-10-10 05:08:41 +0530

    add logging statement

--- compile.js
+++ compile.js
@@ -4,7 +4,7 @@ const solc = require("solc");
 
 const Route = path.resolve(__dirname, "contracts", "Route.sol");
 const source = fs.readFileSync(Route, "utf-8");
-// console.log(solc.compile(source, 2));
+console.log(solc.compile(source, 2));
 module.exports = {
   route: solc.compile(source, 2).contracts[":Route"],
   routeCreator: solc.compile(source, 2).contracts[":RouteCreator"]
